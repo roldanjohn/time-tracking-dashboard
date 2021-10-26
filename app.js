8f7de34bd68a4f4528a6adc83b83ff9b21a99d71
roldanjohn <roldanjohnpatrick@gmail.com>
2021-10-26 12:10:10 +0800

    Update app.js
    I just moved the code to its own function.

--- app.js
+++ app.js
@@ -12,36 +12,35 @@ getUserData().then(data => stats = data);
 
 
 links.forEach(link => {
-  link.addEventListener('click', (e) => {
-    links.forEach(x => {
-      x.classList.remove('active');
-    });
-    e.target.classList.add('active');
-    let timespan = e.target.getAttribute('data-id');
-    let previous = timespan == 'daily' ? 'Yesterday' : timespan == 'weekly' ? 'Last Week': 'Last Month';
-    stats.forEach(stat => {
-      timeDivs.forEach(time => {
-        if(stat.title.toLowerCase() == time.getAttribute('data-id')){
-          // console.log(stat.timeframes[timespan].current);
-          let currentTime = stat.timeframes[timespan].current;
-          let lastTime = stat.timeframes[timespan].previous;
-          let hrC = currentTime === 1 ? 'hr':'hrs' ;
-          let hrP = lastTime === 1 ? 'hr':'hrs' ;
-          time.innerHTML = `
-          <div class="activity-div">
-          ${stat.title}
-          <img src="/images/icon-ellipsis.svg" alt="">
-          </div>
-          <h2 class="time-taken"> ${currentTime}${hrC}</h2>
-          <p class="previous-time">${previous} - ${lastTime}${hrP}</p>
-          `;
-
-        }
-      })
-    })
-  })
+  link.addEventListener('click', showStats);
 })
 
-function showStats(timeframe){
 
+function showStats(e){
+  links.forEach(x => {
+    x.classList.remove('active');
+  });
+  e.target.classList.add('active');
+  let timespan = e.target.getAttribute('data-id');
+  let previous = timespan == 'daily' ? 'Yesterday' : timespan == 'weekly' ? 'Last Week': 'Last Month';
+  stats.forEach(stat => {
+    timeDivs.forEach(time => {
+      if(stat.title.toLowerCase() == time.getAttribute('data-id')){
+        // console.log(stat.timeframes[timespan].current);
+        let currentTime = stat.timeframes[timespan].current;
+        let lastTime = stat.timeframes[timespan].previous;
+        let hrC = currentTime === 1 ? 'hr':'hrs' ;
+        let hrP = lastTime === 1 ? 'hr':'hrs' ;
+        time.innerHTML = `
+        <div class="activity-div">
+        ${stat.title}
+        <img src="/images/icon-ellipsis.svg" alt="">
+        </div>
+        <h2 class="time-taken"> ${currentTime}${hrC}</h2>
+        <p class="previous-time">${previous} - ${lastTime}${hrP}</p>
+        `;
+
+      }
+    })
+  })
 }
